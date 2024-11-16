04a3f97761cb58b905f0b6280d39ef18c14623ce
HusenKomilov <husenkomilov73@gmail.com>
2024-11-16 16:50:15 +0500

    feat: car and comment api

--- cars/urls.py
+++ cars/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from cars import views
 
 urlpatterns = [
-    path("cars/", views.CarListAPIView.as_view(), name="cars_list"),
-    path("comments/<int:id>/", views.CommentListAPIView().as_view(), name="comments_list_create"),
+    path("", views.CarListAPIView.as_view(), name="cars_list"),
+    path("<int:pk>/", views.CarRetrieveUpdateDestroyAPIView.as_view(), name="cars_retrieve"),
+    path("<int:pk>/comments/", views.CommentListAPIView().as_view(), name="comments_list_create"),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,19 +1,33 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import NotFound
 
 from cars.models import Car, Comment
 from .serializers import CarSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
-class CarListAPIView(generics.ListAPIView):
+class CarListAPIView(generics.ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
 
-class CommentListAPIView(generics.ListAPIView):
+class CarRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    http_method_names = ["get", "delete", "put"]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentListAPIView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        car_id = self.kwargs.get("id")
+        car_id = self.kwargs['pk']
         return Comment.objects.filter(car=car_id)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
